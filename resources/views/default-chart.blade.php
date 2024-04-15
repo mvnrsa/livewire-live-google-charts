b0af883c392e71ff90794c05e5051ffc16b4c33c
Marnus van Niekerk <mvn@mjvn.net>
2024-04-15 10:10:50 +0200

    Fix traling ;

--- resources/views/default-chart.blade.php
+++ resources/views/default-chart.blade.php
@@ -7,7 +7,7 @@
 		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 		<script type="text/javascript">
 			google.charts.load( 'current', { 'packages':[ 'corechart' ], 'language': '{{ app()->getLocale() }}' } );
-		</script>';
+		</script>
 	@endonce
 
 	<div>
